abe5f622e5120217f37a4f943bce75a6c27eb38d
Adedamola Adedapo <adedamola@sendme.ng>
2023-02-27 12:35:12 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,10 @@
 echo "installing dependencies..."
 pip install -r requirements.txt
 
-echo "Starting up project..."
+echo "Running test..."
+python manage.py test
+
+echo "Starting the service server..."
 echo "Vist: http://127.0.0.1:8000/docs/ in your preferred browser."
 python manage.py runserver
 
